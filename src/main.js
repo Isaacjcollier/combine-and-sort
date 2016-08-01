77f451895bbd8f8f0c4467f45c6e05fccc1a753a
Isaacjcollier <isaacjcollier@gmail.com>
2016-08-01 09:53:38 -0600

    warmup for the day

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ function alphabatize (name1, name2){
   var combinedNameList = name1.concat(name2);
   return combinedNameList.sort();
 }
+
 console.log(alphabatize(nameListOne, nameListTwo));
 console.log('Original List # 1', nameListOne);
 console.log('Original List # 2', nameListTwo);
